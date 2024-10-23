44a4779fe8bfaaa6bc6c312a6eb8aa4d9eb7732f
Andrija Macek <andrija.macek@fer.unizg.hr>
2024-10-23 21:28:22 +0200

    Add initial files for backend

---
+++ backend/src/test/java/hr/unizg/fer/razmjenaUlaznica/RazmjenaUlaznicaApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.unizg.fer.razmjenaUlaznica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RazmjenaUlaznicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
