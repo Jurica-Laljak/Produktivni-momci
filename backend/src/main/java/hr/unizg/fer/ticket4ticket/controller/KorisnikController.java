c9a43b955d64c0a6214e043ec7473185e81b7511
Andrija Macek <andrija.macek@fer.unizg.hr>
2024-12-28 16:56:45 +0100

    Novi endpoint za podatke o korisniku

--- backend/src/main/java/hr/unizg/fer/ticket4ticket/controller/KorisnikController.java
+++ backend/src/main/java/hr/unizg/fer/ticket4ticket/controller/KorisnikController.java
@@ -1,18 +1,11 @@
 package hr.unizg.fer.ticket4ticket.controller;
 
 import hr.unizg.fer.ticket4ticket.dto.KorisnikDto;
-import hr.unizg.fer.ticket4ticket.service.JwtTokenService;
 import hr.unizg.fer.ticket4ticket.service.KorisnikService;
-import hr.unizg.fer.ticket4ticket.utils.CookieUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.view.RedirectView;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
@@ -31,9 +24,15 @@ public class KorisnikController {
     }
 
     // Returns the Korisnik by specified ID
-    @GetMapping("{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<KorisnikDto> getKorisnikById(@PathVariable("id") Long korisnikId){
         KorisnikDto korisnikDto= korisnikService.getKorisnikById(korisnikId);
+        return ResponseEntity.ok(korisnikDto);
+    }
+
+    @GetMapping("/g/{googleId}")
+    public ResponseEntity<KorisnikDto> getKorisnikByGoogleId(@PathVariable("googleId") String googleId){
+        KorisnikDto korisnikDto = korisnikService.findKorisnikByGoogleId(googleId, new KorisnikDto());
         return ResponseEntity.ok(korisnikDto);
     }
 
